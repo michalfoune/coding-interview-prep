da3c3be921cbb781b49cadddc7dcec9739dd51f3
“michalfoune” <michal.foune@gmail.com>
2026-06-03 14:27:53 -0700

    Group Anagram solution.

---
+++ neetcode/arrays-and-hashing/groupAnagram.py
@@ -0,0 +1,28 @@
+"""
+Problem: Group Anagrams
+Pattern: Hash Map / Sorting
+
+Time: O(n * k log k)
+Space: O(n * k)
+
+Where:
+n = number of words
+k = maximum length of a word
+
+Idea:
+Sort each word to create a canonical key.
+Words with the same canonical key are anagrams and belong in the same group.
+"""
+
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+
+        for s in strs:
+            canonical_key = ''.join(sorted(s))
+            groups[canonical_key].append(s)
+
+        return list(groups.values())
